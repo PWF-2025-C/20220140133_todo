ec2fdafc287837a2edd396f0b355b4c07430d039
DaffaPutra7 <putradika3604@gmail.com>
2025-03-15 16:25:30 +0700

    env, mvc, route, relationship

---
+++ tests/Feature/EnvironmentTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class EnvironmentTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function testEnv()
+    {
+        $appName = env("YOUTUBE");
+
+        self::assertEquals("Programmer Zaman Now", $appName);
+    }
+}
