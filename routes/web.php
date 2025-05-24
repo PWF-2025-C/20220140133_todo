41b608cdf2018f4d34ba335e3460fc3b25835657
DaffaPutra7 <putradika3604@gmail.com>
2025-05-24 15:39:10 +0700

    mailtrap

--- routes/web.php
+++ routes/web.php
@@ -14,24 +14,24 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth', 'verified')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/todo', [TodoController::class, 'index'])->name('todo.index');
-    Route::post('/todo', [TodoController::class, 'store'])->name('todo.store');
-    Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
-    Route::get('/todo/edit', [TodoController::class, 'edit'])->name('todo.edit');
+    //Route::get('/todo', [TodoController::class, 'index'])->name('todo.index');
+    //Route::post('/todo', [TodoController::class, 'store'])->name('todo.store');
+    //Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
+    //Route::get('/todo/edit', [TodoController::class, 'edit'])->name('todo.edit');
 
     Route::resource('todo', TodoController::class)->except(['show']);
     Route::patch('/todo/{todo}/complete', [TodoController::class, 'complete'])->name('todo.complete');
     Route::patch('/todo/{todo}/uncomplete', [TodoController::class, 'uncomplete'])->name('todo.uncomplete');
 
-    Route::get('/todo/{todo}/edit',[TodoController::class, 'edit'])->name('todo.edit');
-    Route::get('/todo/{todo}',[TodoController::class, 'update'])->name('todo.update');
+    //Route::get('/todo/{todo}/edit',[TodoController::class, 'edit'])->name('todo.edit');
+    //Route::get('/todo/{todo}',[TodoController::class, 'update'])->name('todo.update');
 
-    Route::delete('/todo/{todo}', [TodoController::class, 'destroy'])->name('todo.destroy');
+    //Route::delete('/todo/{todo}', [TodoController::class, 'destroy'])->name('todo.destroy');
     Route::delete('/todo', [TodoController::class, 'destroyCompleted'])->name('todo.deleteallcompleted');
 
     Route::resource('categories', CategoryController::class)->except(['show']);
---
+++ tugas10.md
@@ -0,0 +1,13 @@
+# Verified Email : Mailtrap
+
+## Browser Register
+![Browser Register](screenshot/tugas10/browserRegister.png)
+
+## Browser Verify Email
+![Browser Verify Email](screenshot/tugas10/browserVerifyEmail.png)
+
+## Browser Mailtrap
+![Browser Mailtrap](screenshot/tugas10/browserMailtrap.png)
+
+## Browser Dashboard
+![Browser Dashboard](screenshot/tugas10/browserDashboard.png)
